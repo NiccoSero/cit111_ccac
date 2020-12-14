cf15a44c949d26af8671cb99912abb42dccae841
NiccoSero <72184288+NiccoSero@users.noreply.github.com>
2020-12-13 20:15:07 -0500

    Add files via upload

---
+++ Object Project/SUV.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+/**
+ *
+ * @author Nicco
+ */
+public class SUV {
+    
+    // create string variable to inform user of car types
+    public String SUVOption1 = "This vehicle is a red SUV.";
+    
+    public String SUVOption2 = "This vehicle is a black SUV.";
+    
+    public String SUVOption3 = "This vehicle is a blue SUV.";
+    
+    public String SUVOption4 = "this vehicle is a purple SUV.";
+    
+    public String SUV;
+    
+} // close class
---
+++ Object Project/Truck.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+/**
+ *
+ * @author Nicco
+ */
+public class Truck {
+    
+    // create string variable to inform user of car types
+    public String truckOption1 = "This vehicle is a red truck.";
+    
+    public String truckOption2 = "This vehicle is a black truck.";
+    
+    public String truckOption3 = "This vehicle is a blue truck.";
+    
+    public String truckOption4 = "this vehicle is a purple truck.";
+    
+    public String Truck;
+    
+} // close class
